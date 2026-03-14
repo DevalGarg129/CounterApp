afd6e7603c7689ade2f337d14bf311817a62107f
devalgarg129 <devalgarg978>
2026-03-14 15:11:05 +0530

    first commit

---
+++ started/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Counter from "./Components/counter";
+export default function App() {
+  return (
+    <div>
+      <h1>React Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+     
